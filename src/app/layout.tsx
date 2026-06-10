6ce063f8f8bc926fc063f0626f5f9fcd4c31057d
canberk_yildiz <canberkyildiz@mail.ru>
2026-06-10 10:12:11 +0300

    fix: update Yandex Webmaster verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,7 +91,7 @@ export const metadata: Metadata = {
   },
   verification: {
     google: 'soUkwfns4jPrW0HEU8FPNe80U_FGTtSl9CG82LDQJMU',
-    yandex: 'b0fb4d9f95168d5a',
+    yandex: '79961fae532083c7',
   },
   alternates: {
     canonical: 'https://www.infodaily.net',
